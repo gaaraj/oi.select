d9cf7837b1697fabe687fb1fc71193c7cebdc4b9
Üsame Fethullah AVCI <u@gaaraj.com>
2016-11-12 12:02:16 +0300

    gulpfile index fix & src files converted single file

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,82 @@
-var path          = require('path');
-var del           = require('del');
-var series        = require('stream-series');
-var gulp          = require('gulp');
-var rename        = require('gulp-rename');
-var concat        = require('gulp-concat');
-var webserver     = require('gulp-webserver');
+var path = require('path');
+var del = require('del');
+var series = require('stream-series');
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var webserver = require('gulp-webserver');
 var templateCache = require('gulp-angular-templatecache');
-var minifyCss     = require('gulp-minify-css');
-var minifyHtml    = require('gulp-minify-html');
-var uglify        = require('gulp-uglify');
-var stylus        = require('gulp-stylus');
+var minifyCss = require('gulp-minify-css');
+var minifyHtml = require('gulp-minify-html');
+var uglify = require('gulp-uglify');
+var stylus = require('gulp-stylus');
 // var KarmaServer   = require('karma').Server;
-var autoprefixer  = require('autoprefixer-stylus')({
-    browsers: ["ff >= 20", "chrome >= 35", "safari >= 7", "ios >= 7", "android >= 4", "opera >= 12.1", "ie >= 10"]
+var autoprefixer = require('autoprefixer-stylus')({
+  browsers: ["ff >= 20", "chrome >= 35", "safari >= 7", "ios >= 7", "android >= 4", "opera >= 12.1", "ie >= 10"]
 });
 
 var paths = {
-    root:     __dirname,
-    src:      path.join(__dirname, '/src'),
-    dist:     path.join(__dirname, '/dist')
+  root: __dirname,
+  src: path.join(__dirname, '/src'),
+  dist: path.join(__dirname, '/dist')
 };
 
 gulp.task('webserver', function() {
-    return gulp.src(paths.root)
-        .pipe(webserver({
-            host: 'localhost',
-            port: 3000,
-            fallback: 'index.html',
-            livereload: true
-        }));
+  return gulp.src(paths.root)
+    .pipe(webserver({
+      host: 'localhost',
+      port: 3000,
+      fallback: 'index.html',
+      livereload: true
+    }));
 });
 
 gulp.task('clean', function() {
-    del.sync(paths.dist);
+  del.sync(paths.dist);
 });
 
 gulp.task('compileStyles', function() {
-    return gulp.src(path.join(paths.src, 'style.styl'))
-        .pipe(stylus({
-            use: autoprefixer
-        }))
-        .pipe(concat('select.css'))
-        .pipe(gulp.dest(paths.dist))
-        .pipe(minifyCss())
-        .pipe(rename('select.min.css'))
-        .pipe(gulp.dest(paths.dist))
+  return gulp.src(path.join(paths.src, 'style.styl'))
+    .pipe(stylus({
+      use: autoprefixer
+    }))
+    .pipe(concat('select.css'))
+    .pipe(gulp.dest(paths.dist))
+    .pipe(minifyCss())
+    .pipe(rename('select.min.css'))
+    .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('compileScripts', function() {
-    var templateStream = gulp.src(path.join(paths.src, 'template.html'))
-        .pipe(minifyHtml())
-        .pipe(templateCache({
-            module: 'oi.select',
-            root: 'src/'
-        }));
+  var templateStream = gulp.src(path.join(paths.src, 'template.html'))
+    .pipe(minifyHtml())
+    .pipe(templateCache({
+      module: 'oi.select',
+      root: 'src/'
+    }));
 
-    var scriptStream = gulp.src([
-        path.join(paths.src, 'module.js'),
-        path.join(paths.src, 'services.js'),
-        path.join(paths.src, 'directives.js'),
-        path.join(paths.src, 'filters.js')
-    ]);
+  var scriptStream = gulp.src(path.join(paths.src, 'module.js'));
+  scriptStream
+    .pipe(concat('select.js'))
+    .pipe(gulp.dest(paths.dist))
+    .pipe(uglify())
+    .pipe(rename('select.min.js'))
+    .pipe(gulp.dest(paths.dist));
 
-    scriptStream
-        .pipe(concat('select.js'))
-        .pipe(gulp.dest(paths.dist))
-        .pipe(uglify())
-        .pipe(rename('select.min.js'))
-        .pipe(gulp.dest(paths.dist));
-
-    series(scriptStream, templateStream)
-        .pipe(concat('select-tpls.js'))
-        .pipe(gulp.dest(paths.dist))
-        .pipe(uglify())
-        .pipe(rename('select-tpls.min.js'))
-        .pipe(gulp.dest(paths.dist));
+  series(scriptStream, templateStream)
+    .pipe(concat('select-tpls.js'))
+    .pipe(gulp.dest(paths.dist))
+    .pipe(uglify())
+    .pipe(rename('select-tpls.min.js'))
+    .pipe(gulp.dest(paths.dist));
 });
 
 gulp.task('watch', function() {
-    gulp.watch(path.join(paths.src, '**/*.styl'), ['compileStyles']);
+  gulp.watch(path.join(paths.src, '**/*.styl'), ['compileStyles']);
 });
 
 gulp.task('watch:serve', function() {
-    gulp.watch(path.join(paths.src, '**/*.js'), ['compileScripts']);
+  gulp.watch(path.join(paths.src, '**/*.js'), ['compileScripts']);
 });
 
 // gulp.task('test', function(done) {
@@ -94,4 +88,3 @@ gulp.task('watch:serve', function() {
 gulp.task('build', ['clean', 'compileScripts', 'compileStyles']);
 gulp.task('serve', ['webserver', 'watch:serve']);
 gulp.task('default', ['webserver', 'watch']);
-
